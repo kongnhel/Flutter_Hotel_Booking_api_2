709f44c442e422e027f673d7ca394afcf1a07c0c
KongNhel <kabkong94@gmail.com>
2025-07-31 13:34:34 +0700

    fg

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./hotelbooking.json");
+// const serviceAccount = require("./hotelbooking.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIAL);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
